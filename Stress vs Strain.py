fdede74ae86b37ddba8175955f7d1aac0f12e76e
Brady6483 <bxh442@francis.edu>
2026-09-21 14:51:46 -0400

    Added Stress vs Strain.py for toughness estimation directly in github
    Add script to calculate toughness from stress-strain data

---
+++ Stress vs Strain.py
@@ -0,0 +1,13 @@
+import numpy as np
+filename = r'C:\Users\chunk\OneDrive\Desktop\ENGR210 Fa2026 HW04 Prob2 Data(in).csv'
+data = np.loadtxt(filename, delimiter=',', skiprows=1)
+strain = data[:, 0]
+stress = data[:, 1]
+toughness = 0
+i = 0
+while i < len(strain) - 1:
+    dx = strain[i+1] - strain[i]
+    avg_y = (stress[i] + stress[i+1]) / 2
+    toughness += avg_y *dx
+    i = i + 1
+print("Estimated Toughness:", toughness)
